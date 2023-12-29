15a059e50ed9b4cfe1b37fc5d0d9c36bf4cd21fe
jalil betroji <jalil.betroji@gmail.com>
2023-12-29 13:28:38 +0100

    project repository added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProjectsController;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('Projects.index');
-});
+Route::get('/', [ProjectsController::class, 'index'])->name('home');
