60a42203f487cb9b1d951c81a11434de86f5a142
jalil betroji <jalil.betroji@gmail.com>
2024-01-03 02:08:14 +0100

    views updated

--- resources/views/Tasks/show.blade.php
+++ resources/views/Tasks/show.blade.php
@@ -1,38 +1,50 @@
-@extends('../Layouts.Layout')
+@extends('layouts.layout')
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Details de la tâche :</h1>
-                </div>
+<div class="wrapper">
 
-            </div>
-        </div>
-    </div>
-
-
-
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-6 offset-md-3">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $task->name }}</h5>
-                        <p class="card-text">{{ $task->description }}</p>
-                        <div class="tags mt-3">
-                            <span class="badge badge-primary">{{ $task->project->name }}</span>
+        <div class="content-wrapper" style="min-height: 1302.4px;">
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Détails du projet</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="./edit.php" class="btn btn-default float-right"><i class="far fa-edit"></i> Modifier</a>
                         </div>
                     </div>
                 </div>
-                <div class="row ">
-                    <div class="d-flex justify-content-end ">
-                        <a href="{{ route('index') }}" class="btn btn-md btn-secondary">Retour</a>
+            </div>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="col-sm-12">
+                                        <label for="nom">Nom:</label>
+                                        <p>Projet 1</p>
+                                    </div>
+
+                                    <!-- Description Field -->
+                                    <div class="col-sm-12">
+                                        <label for="description">Description:</label>
+                                        <p>Description du projet 1.</p>
+                                    </div>
+
+                                    <!-- Description Field -->
+                                    <div class="col-sm-12">
+                                        <label for="description">Date:</label>
+                                        <p>Date de début: 11-7-2014</p>
+                                        <p>Date de fin: 11-7-2014</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </section>
+
         </div>
     </div>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 @endsection
